82752069eb3df34108e42c95eebcf4ad2fdc35fa
Steven Winston <swinston100@hotmail.com>
2017-02-28 10:18:50 +0200

    several fixes

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ var app = express();
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
 
-app.listen(8000);
+app.listen(8000, function() {
+  console.log("Fullstack project. Listening on 8000.")
 
-app.get('/', function (req, res) {
-  res.send("You are inside the fullstack project")
 });
