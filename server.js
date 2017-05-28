865fa4f6cd9a1da58fb379b85cc103ea6c4a3270
“yenkelgit config --global user.namgit config --global user.name “yenkel <yenkel.mayost@gmail.com>
2017-05-28 16:08:22 +0300

    new repository

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ var mongoose = require('mongoose');
 var LocalStrategy = require('passport-local').Strategy;
 var expressSession = require('express-session')
 var passport = require('passport')
-var userRoutes = require ('./routes/userRoutes')
+var userRoutes = require('./routes/userRoutes')
 var Beer = require("./models/BeerModel");
 var User = require("./models/UserModel");
-mongoose.connect(process.env.CONNECTION_STRING||'mongodb://localhost/beers');
+mongoose.connect(process.env.CONNECTION_STRING || 'mongodb://localhost/beers');
 
 var beerRoutes = require('./routes/beerRoutes');
 var bodyParser = require('body-parser')
@@ -35,4 +35,4 @@ app.use('/users', userRoutes);
 // app.get('/', function(req, res) {
 //   res.send('Testing Server')
 // })
-app.listen(process.env.PORT || '8080');
+app.listen(process.env.PORT || '8000');
